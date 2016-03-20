3be4b509616438dca277ed82423b7787cf50d6e1
kxoloo <v.i.n.y.a.n@hotmail.com>
2016-03-20 23:50:24 +0200

    asdf
    sadf

--- forc.js
+++ forc.js
@@ -5,6 +5,8 @@
  */
 
 var elemID = "#disp";
+var timer;
+var id = 0;
 
 $().ready (function() {
     var top;
@@ -14,4 +16,13 @@ $().ready (function() {
         top = Math.floor((Math.random()*800)+1);
         $(elemID).append("<div id='" + i + "' class='b' style='top:" + top + "px; left:" + left + "px;'>" + i + "</div>");
     }
+	timer = setInterval(function() {
+		for(var i=0; i<10; i++ ) {
+        left = Math.floor((Math.random() * 1000) + 1);
+        top = Math.floor((Math.random() * 800) + 1);        
+        $('#' + i ).css("left",left+"px");
+        $('#' + i ).css("top",top+"px");
+        $('#' + i ).html(id++);
+		}},2000);
+	
 });
